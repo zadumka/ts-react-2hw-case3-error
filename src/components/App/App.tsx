923310a7cd3b854b7b79b7160759d548c6f18648
zadumka <90329284+zadumka@users.noreply.github.com>
2025-04-14 17:23:37 +0300

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -36,7 +36,7 @@ export default function App() {
 
   return (
     <div className={css.app}>
-      <CafeInfo />
+      
       <VoteOptions
         onVote={handleVote}
         onReset={resetVotes}
